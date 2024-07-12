9f12d6bc74eeb88440519436d9833e9834de127c
Alex Santos <alexjosedosantos@gmail.com>
2024-07-11 22:04:22 -0300

    added CrossOrigin - controller

--- src/main/java/com/crud/crud_angular_spring/controller/StudentController.java
+++ src/main/java/com/crud/crud_angular_spring/controller/StudentController.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/v1/students")
 public class StudentController {
 
